733a87842b5d742731f38c14d2bd9971fe01ddd2
LukaszMalecki <lukmal2731@gmail.com>
2024-05-17 14:55:44 +0200

    - Added TranslatorDbContext

---
+++ Afs.Translator/Data/ModelBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Afs.Translator.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Afs.Translator.Data
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Translation>().HasData
+            (
+                new Translation()
+                {
+                    Id = ModelConstants.DefaultTranslationId,
+                    TranslationName = ModelConstants.DefaultTranslation
+                }
+                
+            );
+        }
+    }
+}
---
+++ Afs.Translator/Data/TranslatorDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Afs.Translator.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Afs.Translator.Data
+{
+    public class TranslatorDbContext : DbContext
+    {
+        public TranslatorDbContext(DbContextOptions<TranslatorDbContext> options) : base(options) { }
+        public DbSet<TranslationRequest> TranslationRequests { get; set; }
+        public DbSet<TranslationResponse> TranslationResponses { get; set; }
+        public DbSet<Translation> Translations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Seed();
+        }
+    }
+}
